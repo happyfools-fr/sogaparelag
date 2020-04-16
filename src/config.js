94cef7b01421917baf0ecb58e766bbc9171bc708
mf-letourneur <marc.letourneur@mybraintech.com>
2020-04-16 21:21:30 +0200

    add firebase configs in .env

--- src/config.js
+++ src/config.js
@@ -1,14 +1,13 @@
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBt_7TYAZmIQ5xZtNTOE88FF23bazvn4-E",
-  authDomain: "galerapagos-f5bd9.firebaseapp.com",
-  databaseURL: "https://galerapagos-f5bd9.firebaseio.com",
-  projectId: "galerapagos-f5bd9",
-  storageBucket: "galerapagos-f5bd9.appspot.com",
-  messagingSenderId: "537433033375",
-  appId: "1:537433033375:web:664c64c8066139e397f0aa",
-  measurementId: "G-02X58B4KQV"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_MEASUREMENT_ID,
 };
-
 firebase.initializeApp(firebaseConfig);
