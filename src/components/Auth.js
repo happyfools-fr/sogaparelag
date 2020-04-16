8fafb4759492577e56936269862a0784ffe8e784
thomasgrnr <thomasgarnier92@gmail.com>
2020-04-16 14:19:52 +0000

    Cleaning repo & applying structure

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import * as firebase from 'firebase';
 
-function Auth() {
-    return "<input type='email' placeholder='Email'>" ;
-};
+class Auth extends Component {
+    
+    // auth = firebase.Auth();
+
+    render() {
+        return (
+            <div>
+                <div>
+                     <input type='email' placeholder='Email' />
+                </div>
+                <div>
+                    <input type='password' placeholder='Password' />
+                </div>
+                <div>
+                    <Button variant="primary">Log in</Button>
+                    <Button variant="secondary">Sign up</Button>
+                </div>
+                <div>
+                    <Button variant="secondary"> Log in with Google</Button>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Auth;
