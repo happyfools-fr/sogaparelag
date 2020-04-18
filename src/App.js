edb5cb2db62c62cb5c48e56c50484618f0c61ddb
mf-letourneur <marc.letourneur@mybraintech.com>
2020-04-18 22:34:17 +0200

    experiment routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 
@@ -8,6 +14,7 @@ import withFirebaseAuth from 'react-with-firebase-auth';
 import Auth from './components/Auth'
 import GameMenu from './components/GameMenu'
 import LandingPage from './components/LandingPage'
+import About from './components/About'
 
 const firebaseAppAuth = firebaseApp.auth();
 const providers = {
@@ -39,30 +46,74 @@ class App extends Component {
     });
   }
 
-  render() {
-    const {
-      user,
-      signOut,
-      signInWithGoogle,
-    } = this.props;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <LandingPage />
-          <Auth
-            user={user}
-            signOut={signOut}
-            signInWithGoogle={signInWithGoogle}
-          />
-          <GameMenu user={user}/>
-        </header>
+render() {
+  const {
+    user,
+    signOut,
+    signInWithGoogle,
+  } = this.props;
 
-        <footer>
-          <b>Sogaparelag</b> project, a HappyFools.fr initiative
-        </footer>
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          {
+            user
+            ? (
+              <li>
+              <Link to="/game">Game</Link>
+              </li>
+            )
+            : (<div></div>)
+          }
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/login">
+            <Auth
+              user={user}
+              signOut={signOut}
+              signInWithGoogle={signInWithGoogle}
+            />
+          </Route>
+          {
+            user
+            ? (
+              <Route path="/game">
+                <GameMenu user={user} />
+              </Route>
+            )
+            : (<div></div>)
+          }
+        </Switch>
       </div>
+    </Router>
     );
-  };
+  }
 }
 
 export default createComponentWithAuth(App);
