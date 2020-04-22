1c3e0ee92f7e569736267b544f02770dd1e228fd
thomasgrnr <thomasgarnier92@gmail.com>
2020-04-22 16:05:34 +0000

    Created a GameTableView

--- src/apps/game/components/GameStateTable.js
+++ src/apps/game/components/GameStateTable.js
@@ -4,15 +4,12 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase';
 import firebaseApp from '../../../firebaseApp';
 
-// Styles imports
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Jumbotron, Table}  from 'react-bootstrap';
-
 // Model
 import Game from '../model/Game';
 
 // Views
 import WaitingRoomView from '../views/WaitingRoomView';
+import GameTableView from "../views/GameTableView";
 
 const db = firebase.firestore(firebaseApp);
 
@@ -81,65 +78,16 @@ export default class GameStateTable extends Component {
         Game.startFirstRound(game);
         alert("Game.startFirstRound");
     };
-  
-  createJumbotron(game) {
-    if (!game.currentState.isStarted){
-
-        return ( <WaitingRoomView game={game} onClick={this.onClickNotStarted} />);
-
-    } else {
-      return (
-        <Jumbotron>
-          <h1>{game.slugname.toUpperCase()} </h1>
-          <p>
-            A simple jumbotron-style component to display the game state info.
-          </p>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Round #</th>
-                <th>Total Moves #</th>
-                <th>Current Player</th>
-                <th>Next Player</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{game.currentState.roundNumber}</td>
-                <td>{game.history.length}</td>
-                <td>{this.state.currentPlayerNickname}</td>
-                <th>{this.state.nextPlayerNickname}</th>
-              </tr>
-            </tbody>
-        </Table>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Total Water Supply</th>
-              <th>Total Food Supply</th>
-              <th>Total Wood Supply</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{game.currentState.waterSupply}</td>
-              <td>{game.currentState.foodSupply}</td>
-              <td>{game.currentState.woodSupply}</td>
-            </tr>
-          </tbody>
-      </Table>
-        </Jumbotron>
-     );
-   }
- }
  
   render() {
-    return (
-      <React.Fragment>
-        { 
-          this.createJumbotron(this.props.game) 
-        }
-      </React.Fragment>
-
-  )}
+    const game = this.props.game;
+    if (!game.currentState.isStarted){
+        return ( <WaitingRoomView game={game} onClick={this.onClickNotStarted} />);
+    } else {
+      return ( <GameTableView game={game}
+        currentPlayerNickname={this.state.currentPlayerNickname}
+        nextPlayerNickname={this.state.nextPlayerNickname}
+        />);
+    };
+  };
 }
