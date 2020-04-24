b591299b70c977ba323789f0fcfabda7087420eb
thomasgrnr <thomasgarnier92@gmail.com>
2020-04-24 19:30:40 +0200

    GameView as a component + close turn modal + list players in game

--- src/apps/game/views/WaitingRoomView.js
+++ src/apps/game/views/WaitingRoomView.js
@@ -6,7 +6,10 @@ import {Jumbotron, Button}  from 'react-bootstrap';
 export default function WaitingRoomView(props) {
     return (
         <Jumbotron>
-          <h3>Welcome to {props.game.slugname}!</h3>
+          <h3>Welcome to {props.gameSlugname}!</h3>
+          {
+              props.players.map((player) => {return (<p>{player}</p>)})
+          }
           <div>
             <Button variant="primary" onClick={() => {props.onClick()}}>
                 Start the game if everybody is ready!
