5191ef6d641ff08a2d45830853a36bb92f0a2385
thomasgrnr <thomasgarnier92@gmail.com>
2020-05-01 10:49:33 +0200

    Work In Progress

--- src/apps/game/views/PollModal.js
+++ src/apps/game/views/PollModal.js
@@ -2,19 +2,30 @@ import React, {useState} from 'react';
 
 import {Modal, Form, Button, ToggleButton, ToggleButtonGroup} from 'react-bootstrap';
 
+/**
+*
+*   @params players
+*   @params show
+*   @params handleVote
+*
+*/
 export default function PollModal(props) {
 
     // Set condition
     const show = true
-
-    const players = [{_id:1}, {_id:2}, {_id:3}, {_id:4}, {_id:5} ];
-
-    const [vote, setVote] = useState()
-
-    const handleSubmit = (submit) => {
+    const players = [
+        {_id: 1}, {_id: 2}, {_id: 3}, {_id: 4},
+        {_id: 5}, {_id: 6}, {_id: 7}, {_id: 8},
+        {_id: 9}, {_id: 10}, {_id: 11}, {_id: 12}
+    ];
+    const handleVote = (submit) => {
         // To-Do
         console.log(vote)
     };
+
+    const [vote, setVote] = useState()
+
+
 
     return (
         <Modal show={show} centered>
@@ -23,7 +34,7 @@ export default function PollModal(props) {
             </Modal.Header>
             <Modal.Body>
                 You must choose which player will not drink today :
-                <Form className="mt-2" onSubmit={handleSubmit}>
+                <Form className="mt-2" onSubmit={handleVote}>
                     <Form.Group>
                     {/* How to scale the visual ?? */}
                     <ToggleButtonGroup type="radio" name="players" size="lg" onChange={setVote} toggle>
