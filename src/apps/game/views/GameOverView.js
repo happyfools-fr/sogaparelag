0a41efe917b1aefde051732ecba4d7f16ea4bc9d
thomasgrnr <thomasgarnier92@gmail.com>
2020-05-08 12:17:53 +0200

    Added the button on Saved and GameOver views

--- src/apps/game/views/GameOverView.js
+++ src/apps/game/views/GameOverView.js
@@ -9,11 +9,16 @@ export default function GameOverView(props) {
 
     return (
         <div>
-        <h2 className="mt-4">
-            Game Over ...
-        </h2>
-        <h3> You have all died on the deserted island of {slugname} </h3>
-        <img alt="" className="mt-4 " src={require('../../../assets/tombstone.jpg') } />
+            <h2 className="mt-4">
+                Game Over ...
+            </h2>
+            <h3> You have all died on the deserted island of {slugname} </h3>
+            <div className="m-4">
+                <Button variant="primary" onClick={(click) => {props.handleClickCreateNewGame(click)} }>
+                    Try again - Start a new game
+                </Button>
+            </div>
+            <img alt="" className="mt-4 " src={require('../../../assets/tombstone.jpg') } />
         </div>
     );
 }
